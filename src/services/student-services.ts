037f020f94e9f47214b3bf8ebc7037e78b7861ed
Rclayton999 <64425715+Rclayton999@users.noreply.github.com>
2020-05-07 18:09:02 -0400

    fixed error messages

--- src/services/student-services.ts
+++ src/services/student-services.ts
@@ -1,14 +1,12 @@
 import { Student } from "../models/student";
+import { isNull } from "util";
 
 export function getAllStudents():Student[]{
-
+    return null;
 }
 export function getStudentByID(id :number):Student{
-
-    
-
-    
+    return null;
 }
 export function saveStudent(student):Student{
-
+    return null;
 }
