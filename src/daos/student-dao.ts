8f69bffc6113a4c0631d17fa8e0e8b00c9ff1bcd
Rclayton999 <64425715+Rclayton999@users.noreply.github.com>
2020-05-08 14:46:54 -0400

    protect against sql injection

--- src/daos/student-dao.ts
+++ src/daos/student-dao.ts
@@ -11,13 +11,13 @@ export function getAllStudents(): Promise<Student[]>{
     });
 }
 export function getStudentByID(id: number):Promise<Student>{
-    const sql = `select * from students where sid = ${id}`;
-    return db.query<StudentRow>(sql,[])
+    const sql = `select * from students where sid = $1`;
+    return db.query<StudentRow>(sql,[id])
         .then(result => result.rows.map(r=> Student.from(r))[0]);
 
 }
 export function saveStudent(student: any):Promise<Student>{
-    const sql = `insert into students (first_name,last_name,major) values('${student.first_name}','${student.last_name}','${student.major}') returning *`;
-    return db.query<StudentRow>(sql,[]).then(result => result.rows.map(r=> Student.from(r))[0]);
+    const sql = `insert into students (first_name,last_name,major) values($1,$2,$3) returning *`;
+    return db.query<StudentRow>(sql,[student.first_name,student.last_name,student.major]).then(result => result.rows.map(r=> Student.from(r))[0]);
 
 }
